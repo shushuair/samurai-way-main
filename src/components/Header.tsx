aa31bc7e007c0ac574e594157906169396089c04
shushuair <shushulkov1989@gmail.com>
2023-05-18 09:21:15 +0100

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://i.ebayimg.com/images/g/F7IAAOSwKWBkSmH9/s-l500.png"
+                alt=""/>
+        </header>
+    );
+};
+
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Navbar = () => {
+    return (
+        <nav className="nav">
+            <div>
+                Profile
+            </div>
+            <div>
+                Messages
+            </div>
+            <div>
+                News
+            </div>
+            <div>
+                Music
+            </div>
+            <div>
+                Settings
+            </div>
+        </nav>
+    );
+};
+
