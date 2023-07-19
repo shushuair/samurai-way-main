1a49a58b1fa29614ecc1ff38943833ff9acfaddb
shushuair <shushulkov1989@gmail.com>
2023-07-19 19:38:35 +0100

    Add functions to state

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,123 +1,95 @@
-
-
-
-import {rerenderEntireTree} from "../render";
-import {v1} from "uuid";
+import { rerenderEntireTree } from "../render";
+import { v1 } from "uuid";
 
 export type StateType = {
-    profilePage: ProfilePageType
-    dialogsPage: DialogsPageType
-}
+    profilePage: ProfilePageType;
+    dialogsPage: DialogsPageType;
+    addPost: ()=>void
+    addPostValueTextarea:(postValue:string)=>void
+    addMessage: ()=>void
+    addMessageValue: (postMessage: string)=>void
+};
 
 export type ProfilePageType = {
-    posts: PostsType[]
-    newPostText: string
-}
+    posts: PostsType[];
+    newPostText: string;
+};
 
 export type DialogsPageType = {
-    dialogs: DialogsType[]
-    messages: MessageType[]
-}
+    dialogs: DialogsType[];
+    messages: MessageType[];
+    newMessageText: string; // Add the missing property here
+};
+
 export type PostsType = {
-    id: string
-    message: string
-    likesCount: number
-}
+    id: string;
+    message: string;
+    likesCount: number;
+};
 
 export type DialogsType = {
-    id: string
-    name: string
-}
+    id: string;
+    name: string;
+};
 
 export type MessageType = {
-    id: string
-    message: string
-}
+    id: string;
+    message: string;
+};
 
-let state = {
+let state: StateType = {
     profilePage: {
         posts: [
-            {id: v1(), message: "Hi, how are you? ", likesCount: 10},
-            {id: v1(), message: "I'm fine ", likesCount: 3},
-            {id: v1(), message: "Ohoho ", likesCount: 12},
-            {id: v1(), message: "wow ", likesCount: 5},
+            { id: v1(), message: "Hi, how are you? ", likesCount: 10 },
+            { id: v1(), message: "I'm fine ", likesCount: 3 },
+            { id: v1(), message: "Ohoho ", likesCount: 12 },
+            { id: v1(), message: "wow ", likesCount: 5 },
         ],
-        newPostText: ""
+        newPostText: "",
     },
     dialogsPage: {
         dialogs: [
-            {id: v1(), name: "Ilya"},
-            {id: v1(), name: "Aleks"},
-            {id: v1(), name: "Den"},
-            {id: v1(), name: "Jack"},
-            {id: v1(), name: "Mike"},
-            {id: v1(), name: "Liam"},
+            { id: v1(), name: "Ilya" },
+            { id: v1(), name: "Aleks" },
+            { id: v1(), name: "Den" },
+            { id: v1(), name: "Jack" },
+            { id: v1(), name: "Mike" },
+            { id: v1(), name: "Liam" },
         ],
         messages: [
-            {id: v1(), message: "Hi there"},
-            {id: v1(), message: "Where are you?"},
-            {id: v1(), message: "I am so far"},
-            {id: v1(), message: "Yo"},
-            {id: v1(), message: "No way"},
+            { id: v1(), message: "Hi there" },
+            { id: v1(), message: "Where are you?" },
+            { id: v1(), message: "I am so far" },
+            { id: v1(), message: "Yo" },
+            { id: v1(), message: "No way" },
         ],
-        newMessageText: ""
+        newMessageText: "", // Initialize the newMessageText property
     },
-    addPost: () => {
+    addPost: function () {
         let newPost = {
             id: v1(),
             message: state.profilePage.newPostText,
-            likesCount: 0
-        }
-        state.profilePage.posts.push(newPost)
-        rerenderEntireTree(state)
+            likesCount: 0,
+        };
+        state.profilePage.posts.push(newPost);
+        rerenderEntireTree(state);
     },
-    addPostValueTextarea: (postValue: string) => {
-        state.profilePage.newPostText = postValue
-        rerenderEntireTree(state)
+    addPostValueTextarea: function (postValue: string) {
+        state.profilePage.newPostText = postValue;
+        rerenderEntireTree(state);
     },
-    addMessage: () => {
+    addMessage: function () {
         let newMessage = {
             id: v1(),
-            message: state.dialogsPage.newMessageText
-        }
-        state.dialogsPage.messages.push(newMessage)
-        rerenderEntireTree(state)
+            message: state.dialogsPage.newMessageText,
+        };
+        state.dialogsPage.messages.push(newMessage);
+        rerenderEntireTree(state);
     },
-    addMessageValue: (postMessage: string) => {
-        state.dialogsPage.newMessageText = postMessage
-        rerenderEntireTree(state)
-    }
-}
+    addMessageValue: function (postMessage: string) {
+        state.dialogsPage.newMessageText = postMessage;
+        rerenderEntireTree(state);
+    },
+};
 
-// export let addPost = () => {
-//     let newPost = {
-//         id: v1(),
-//         message: state.profilePage.newPostText,
-//         likesCount: 0
-//     }
-//     state.profilePage.posts.push(newPost)
-//     rerenderEntireTree(state)
-// }
-//
-// export let addPostValueTextarea = (postValue: string) => {
-//     state.profilePage.newPostText = postValue
-//     rerenderEntireTree(state)
-// }
-
-// export let addMessage = () => {
-//     let newMessage = {
-//         id: v1(),
-//         message: state.dialogsPage.newMessageText
-//     }
-//     state.dialogsPage.messages.push(newMessage)
-//     rerenderEntireTree(state)
-// }
-//
-//
-// export let addMessageValue = (postMessage: string) => {
-//     state.dialogsPage.newMessageText = postMessage
-//     rerenderEntireTree(state)
-// }
-
-
-export default state
+export default state;
