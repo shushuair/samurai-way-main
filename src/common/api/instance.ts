8f1b8705e2bdf2299764be406f95aecbd020f154
ILYA SHUSHULKOV <shushulkov1989@gmail.com>
2024-01-10 20:49:44 +0000

    add .env

--- src/common/api/instance.ts
+++ src/common/api/instance.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+// const key = process.env.REACT_APP_API_KEY;
+
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.0",
   withCredentials: true,
   headers: {
-    "API-KEY": "30526515-95f1-4d41-81b7-50437c774f84",
+    "API-KEY": "key",
   },
 });
